f6a009dde8c7ab70fd9547fb003c8cba0e0729ab
tzi ti <ozmax@kurama.(none)>
2014-09-01 21:06:47 +0300

    project base files

---
+++ vigeo/vigeo/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for vigeo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vigeo.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
