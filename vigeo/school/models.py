d98556223bd48e7db68441b78ba205b9ecb1cfab
tzi ti <ozmax@kurama.(none)>
2014-09-14 23:50:59 +0300

    update school models
    add custom user model for student
    and Lesson Question Category

--- vigeo/school/models.py
+++ vigeo/school/models.py
@@ -1,14 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
 
 class Question(models.Model):
     
+    class Meta:
+        permissions = (
+            ("view_admin", "Can see admin pages"),
+            )
+
     title = models.CharField(max_length=200)
     correct_answer = models.CharField(max_length=100)
     possible_answer0 = models.CharField(max_length=100)
     possible_answer1 = models.CharField(max_length=100)
 
 
-    class Meta:
-        permissions = (
-            ("view_admin", "Can see admin pages"),
-            )
+class Student(models.Model):
+     
+    user = models.OneToOneField(User)
+    test_data = models.CharField(max_length=100)
