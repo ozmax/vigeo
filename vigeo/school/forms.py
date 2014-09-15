3758a9ad50266f9075618a7618600cec79628973
tzi ti <ozmax@kurama.(none)>
2014-09-16 01:09:34 +0300

    implement register student form and view

---
+++ vigeo/school/forms.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from django.forms import ModelForm, widgets
+from django.contrib.auth.models import Group
+
+from school.models import Student
+
+class StudentForm(ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'password', 'email']
+        widgets = {'password': widgets.PasswordInput()}
+
+
+    def __init__(self, *args, **kwargs):
+        super(StudentForm, self).__init__(*args, **kwargs)
+        self.fields['username'].help_text = None
+
+    def save(self):
+        user = super(StudentForm, self).save()
+        g = Group.objects.get(name='student') 
+        g.user_set.add(user)
+        Student.objects.create(user_id=user.id)
